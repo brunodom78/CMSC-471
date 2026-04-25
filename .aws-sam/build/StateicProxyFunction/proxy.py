8a005664de4edf6b5ed0bf11b1f6aa4c4791d9f5
Dom Bruno <dombruno@Doms-MacBook-Air.local>
2026-04-25 18:46:26 -0400

    video 4 results

---
+++ .aws-sam/build/StateicProxyFunction/proxy.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+import os
+
+s3 = boto3.client("s3")
+
+def proxy_handler(event, context):
+    # Log the event argument for debugging and for use in local development.
+    print(json.dumps(event))
+
+    bucket = os.environ['BUCKET_BUCKET_NAME']
+
+    response = s3.get_object( Bucket = bucket, Key = 'index.html' )
+    html_body = response['Body'].read().decode('utf-8')
+
+
+    return {
+        'statusCode': 200,
+        'body': html_body,
+        'headers': {
+            "Content-Type": "text/html"
+        }
+    }
